9d2e73a47344f321a21b9a4c344f5ed3091a32b1
John Mettraux <jmettraux@gmail.com>
2012-07-14 07:56:42 +0900

    use RUOTE_STORAGE_DB

--- test/connection.rb
+++ test/connection.rb
@@ -5,32 +5,37 @@
 # Thu Feb 10 11:14:56 JST 2011
 #
 
-require 'yajl' rescue require 'json'
-require 'rufus-json'
-Rufus::Json.detect_backend
-
+require 'rufus-json/automatic'
 require 'ruote-sequel'
+
 
 unless $sequel
 
-  $sequel = Sequel.connect('postgres://localhost/ruote_test')
-
-  #$sequel = Sequel.connect('mysql://root:root@localhost/ruote_test')
-  #$sequel = Sequel.connect('mysql://root@localhost/ruote_test')
+  $sequel = case ENV['RUOTE_STORAGE_DB'] || 'postgres'
+    when 'pg', 'postgres'
+      Sequel.connect('postgres://localhost/ruote_test')
+    when 'my', 'mysql'
+      #Sequel.connect('mysql://root:root@localhost/ruote_test')
+      Sequel.connect('mysql://root@localhost/ruote_test')
+    when /:/
+      Sequel.connect(ENV['RUOTE_STORAGE_DB'])
+    else
+      raise ArgumentError.new("unknown DB: #{ENV['RUOTE_STORAGE_DB'].inspect}")
+  end
 
   Ruote::Sequel.create_table($sequel, true)
     # true forces re_create of 'documents' table
 
   require 'logger'
 
-  logger = nil
-
-  case ENV['RUOTE_STORAGE_DEBUG']
+  logger = case ENV['RUOTE_STORAGE_DEBUG']
     when 'log'
       FileUtils.rm('debug.log') rescue nil
-      logger = Logger.new('debug.log')
+      Logger.new('debug.log')
     when 'stdout'
-      logger = Logger.new($stdout)
+      Logger.new($stdout)
+    else
+      nil
   end
 
   if logger
